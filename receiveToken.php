63c919d0cd52576cdc215eaea0483ba6e3fa4051
Tom Smith <tomgsmith99@gmail.com>
2016-05-29 11:16:39 -0400

    added some error checking

--- receiveToken.php
+++ receiveToken.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "<p>The page is working.";
+$output = "";
+
+if ($_GET["code"]){
+
+	$code = $_GET["code"];
+
+	$output .= "<p>Received token.</p>";
+
+	$output .= "<p>The token is: " . $code;
+
+}
+
+else {
+
+	$output .= "<p>Did not receive a code.</p>";
+
+}
+
+echo $output;
