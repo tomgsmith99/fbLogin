27749b67016fbf0a1bf5c366d8f0922684c88274
Tom Smith <tomgsmith99@gmail.com>
2016-05-29 22:33:07 -0400

    reconfigured logic for checking params

--- receiveToken.php
+++ receiveToken.php
@@ -6,26 +6,25 @@ if ($_GET["code"]){
 
 	$code = $_GET["code"];
 
-	$output .= "<p>Handshake with Facebook was successful.</p>";
+	// $output .= "<p>Handshake with Facebook was successful.</p>";
 
 	$output .= "<p>The code is: " . $code;
+}
 
-	if ($_GET["state"]) {
-		$state = $_GET["state"];
+if ($_GET["state"]) {
+	$state = $_GET["state"];
 
-		$output .= "<p>The state is: " . $state;
-	}
-	if ($_GET["response_type"]) {
-		$response_type = $_GET["response_type"];
+	$output .= "<p>The state is: " . $state;
+}
+if ($_GET["response_type"]) {
+	$response_type = $_GET["response_type"];
 
-		$output .= "<p>The response_type is: " . $response_type;
-	}
-	if ($_GET["access_token"]) {
-		$access_token = $_GET["access_token"];
+	$output .= "<p>The response_type is: " . $response_type;
+}
+if ($_GET["access_token"]) {
+	$access_token = $_GET["access_token"];
 
-		$output .= "<p>The access_token is: " . $access_token;
-	}
-
+	$output .= "<p>The access_token is: " . $access_token;
 }
 
 else {
